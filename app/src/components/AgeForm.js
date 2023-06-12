19eda5472810e3ced77ea7e66530c606afdb3ebd
christmknd <christmokonda@gmail.com>
2023-06-12 17:21:12 +0200

    ADD App.js test

--- app/src/components/AgeForm.js
+++ app/src/components/AgeForm.js
@@ -34,7 +34,7 @@ function AgeForm () {
     }  
 
     return (
-        <div>
+        <div data-testid="age-form">
         <form onSubmit={handleSubmit}>
           <label>
           Entrez votre date de naissance : 
